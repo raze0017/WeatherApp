7a2a9f67c5505b0b9bee5ddb9c83c109be7ea8bc
raze0017 <appurazeem2000@gmail.com>
2024-06-22 15:02:20 +0530

    loaded css and fetching successful

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('HEllo');
+import './styles.css';
+document.addEventListener('DOMContentLoaded',()=>{
+
+
+console.log('HEllOII');
+const search=document.querySelector('#searchField');
+const butt=document.querySelector('.butt');
+
+butt.addEventListener('click',()=>{
+buttOnClick();
+});
+
+function buttOnClick(){
+    fetch(`https://api.weatherapi.com/v1/current.json?key=d5240453a3674fb9819110327242006&q=`+search.value, {mods: 'cors'})
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        console.log(response.current.condition.text);
+    });
+}  
+});   
+ 
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,21 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/,
+    aggregateTimeout: 300,
+    poll: 1000
+  },
+  module: {
+    rules: [
+        { 
+            test: /.css$/,
+            use: ['style-loader', 'css-loader'],
+        },
+    ],
+  },  
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
