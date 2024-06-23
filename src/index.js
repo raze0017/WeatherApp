6d3ad9969704055c88768dd06522472f91b1be22
raze0017 <appurazeem2000@gmail.com>
2024-06-23 17:18:30 +0530

    basic temp info adds when searched for a place

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import './styles.css';
+import './iconstyles.css';
+import { display } from './display';
 document.addEventListener('DOMContentLoaded',()=>{
 
 
 console.log('HEllOII');
 const search=document.querySelector('#searchField');
 const butt=document.querySelector('.butt');
+const img=document.querySelector('img');
+const temp=document.querySelector('.temp');
+const cond=document.querySelector('.cond');
+const minTemp=document.querySelector('.min');
+const maxTemp=document.querySelector('.max');
+const feels=document.querySelector('.feels');
 butt.addEventListener('click',()=>{
 buttOnClick();
 });
@@ -13,11 +21,20 @@ async function buttOnClick(){
     try{
     const response =await fetch(`https://api.weatherapi.com/v1/current.json?key=d5240453a3674fb9819110327242006&q=`+search.value, {mods: 'cors'})
     const weatherData=await response.json();
+    
+    img.src=weatherData.current.condition.icon
+
+    temp.textContent=weatherData.current.temp_c;
+    cond.textContent=weatherData.current.condition.text;
+    minTemp.textContent=weatherData.current.dewpoint_c;
+    maxTemp.textContent=weatherData.current.heatindex_c;
+    feels.textContent=weatherData.current.feelslike_c;
     console.log(weatherData);
     console.log(weatherData.current.condition.text);
     }catch(error){
-        alert('The information is not available! try again with a nearby city or verify the correct spelling', error);
+        alert('The information is not available! try again with a nearby city.', error);
     }
 }  
+buttOnClick();
 });   
  
