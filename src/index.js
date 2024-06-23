d70c08d1b3efa22cf827305e1c7a7309137d6336
raze0017 <appurazeem2000@gmail.com>
2024-06-23 15:33:26 +0530

    added slots for basic info

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded',()=>{
 console.log('HEllOII');
 const search=document.querySelector('#searchField');
 const butt=document.querySelector('.butt');
-
 butt.addEventListener('click',()=>{
 buttOnClick();
 });
@@ -14,6 +13,7 @@ async function buttOnClick(){
     try{
     const response =await fetch(`https://api.weatherapi.com/v1/current.json?key=d5240453a3674fb9819110327242006&q=`+search.value, {mods: 'cors'})
     const weatherData=await response.json();
+    console.log(weatherData);
     console.log(weatherData.current.condition.text);
     }catch(error){
         alert('The information is not available! try again with a nearby city or verify the correct spelling', error);
